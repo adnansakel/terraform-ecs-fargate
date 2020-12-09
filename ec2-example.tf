e857ef4e441f8db6327eedcef45a58a5bf9f2260
Jahidul Adnan Sakel <adnan.sakel@gmail.com>
2020-12-09 20:29:24 +0100

    fixing error

--- ec2-example.tf
+++ ec2-example.tf
@@ -1,16 +1,18 @@
+provider "aws" {
+  region  = "eu-central-1"
+  version = "~> 2.32"
+}
+
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
+  backend "remote" {
+    organization = "test-sakel"
+
+    workspaces {
+      name = "terraform-example"
     }
   }
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
 
 resource "aws_instance" "example" {
   ami           = "ami-830c94e3"
